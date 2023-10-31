ba359e74adbe940250f72c6b0592307fdf2e72a4
Rahul D Shetty <35rahuldshetty@gmail.com>
2023-10-31 15:08:00 +0530

    added dark mode supporot

--- docs/_app/immutable/chunks/index.6110e9c7.js
+++
@@ -1,1 +0,0 @@
-import{U as n}from"./scheduler.8252a539.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
